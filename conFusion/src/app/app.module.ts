979dc2c2bc3b35fad76f162e1ba74c0fff8cccf1
Ganesh Janamanchi <ganeshjanamanchi9@gmail.com>
2018-01-29 21:07:46 +0530

    configuring angularjs and added toolbar

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -2,7 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout'
 
+import 'hammerjs';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -11,8 +15,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
