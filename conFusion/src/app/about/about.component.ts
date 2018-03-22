a1f6a6c8a144f6f522b2f033ea885a89c02b8e5a
Ganesh Janamanchi <ganeshjanamanchi9@gmail.com>
2018-03-23 00:04:56 +0530

    Promises Part 1: used 'Promise' -> getting result without waiting

--- conFusion/src/app/about/about.component.ts
+++ conFusion/src/app/about/about.component.ts
@@ -17,7 +17,8 @@ selectedLeader: Leader;
   constructor(private leaderService: LeaderService) { }
 
   ngOnInit() {
-    this.leaders = this.leaderService.getLeaders();
+    this.leaderService.getLeaders()
+      .then( leaders => this.leaders = leaders );
   }
 
   onSelect(leader: Leader){
